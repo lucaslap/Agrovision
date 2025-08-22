3eeffad12f677dcb3ec8593f36f855340bcce915
Diego Kanamori <diegokanamori@gmail.com>
2025-08-22 12:53:16 -0300

    feat: adicionando footer (#37)
    * feat: criando component footer
    
    * fix: responsividade e alinhamento de conteudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,8 +9,17 @@ function App() {
       <main>
         <div style={{ textAlign: "center", padding: "40px 20px" }}>
           <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
+          <h1>Olá</h1>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
